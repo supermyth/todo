0b23e4c366a6ea803597c259792b5c9ec608139e
supermyth <kimro89@gmail.com>
2015-10-30 17:48:18 +0900

    line add

--- js/main.js
+++ js/main.js
@@ -1,4 +1,56 @@
 $("document").ready(function(){
+	if(!!debug){
+		// debug mode
+	}	
+	if(!!!debug){
+		// not debug mode 
+	}
+	var Env = {
+		id : {
+			memoId : 0,
+			nextMemoId : function(){
+				Env.id.memoId++;
+				return Env.id.memoId;
+			}
+		},
+		add : {
+			addable : true,
+			isAddable : function(){
+				return Env.add.addable;
+			},
+			setAddable : function(status){
+				if(status == true) {
+					Env.add.addable = true;
+				} else if (status == false){
+					Env.add.addable = false;	
+				} else {
+					Env.add.addable = false;
+					EnvFun.cleanUp();
+					Env.add.addable = true;
+				}
+			}
+		},
+		focus : {
+			$el : null,
+			isFocused : function(){
+				return Env.focus.$el !== null;
+			},
+			giveFocus : function($target){
+				if($target !== undefined && $target !== null && $target.hasClass("memo")){
+					Env.focus.$el = $target;
+				}
+			},
+			removeFocus : function(){
+				Env.focus.$el = null;
+			}
+		}
+	};
+	var EnvFun = {
+		cleanUp : function(){
+			// TODO remove textarea focus
+			Env.focus.removeFocus();
+		}
+	};
 	var $memos = $("#memos"),
 		$memoSample = $("#memo_sample");
 	var paddingX = 146, paddingY = 146;
@@ -9,20 +61,58 @@ $("document").ready(function(){
         selectOtherMonths: true,
         dateFormat : "yy-mm-dd"
     });
-	
-	$memos.dblclick(function(e){
-		e.preventDefault();
-		addNewMemo(e.offsetX, e.offsetY);
+	$("body").on({
+		'mouseup' : function(e){
+			if(Env.focus.isFocused()){
+				EnvFun.cleanUp();
+			}
+		}
+	});
+	$memos.on({
+		'mouseup' : function(e) {
+			if(Env.focus.isFocused()){
+				e.stopPropagation();
+				e.preventDefault();
+				var $target = Env.focus.$el;
+				moveTo($target, e.offsetX, e.offsetY);
+// 				$target.css({marginLeft : x, marginTop : y});
+				// TODO redraw if line exists
+				
+			}	
+		},
+		'dblclick' : function(e){
+			e.stopPropagation();
+			e.preventDefault();
+			addNewMemo(e.offsetX, e.offsetY);
+		}
 	});
 	
+	
 	var registEvents = function(){
+		$(".memo").on({
+			'mousedown' : function(e){
+				var $this = $(e.currentTarget);
+				console.log("mouseDown : " + $this.data("id"));
+				Env.focus.giveFocus($this);
+			},
+			'mouseup' : function(e){
+				var $this = $(e.currentTarget);
+				console.log("mouseUp : " + $this.data("id"));
+				if(Env.focus.isFocused() && Env.focus.$el !== $this){
+					drawLine(Env.focus.$el, $this);
+				}
+				Env.focus.removeFocus();
+			}
+		});
 		$(".memo-content").click(function(e){
+			e.stopPropagation();
 			e.preventDefault();
 			var $this = $(e.currentTarget);
 			if($this.parent().data("readonly") !== "true"){
 				var content = $this.find("span").text();
 				$this.find("textarea").val(content);
 				$this.addClass("focus");
+				Env.add.setAddable(false);
 				$this.find("textarea").focus();
 			}
 
@@ -32,8 +122,11 @@ $("document").ready(function(){
 			var content = $this.val();
 			$this.parent().find("span").text(content);
 			$this.parent().removeClass("focus");
+			Env.focus.removeFocus();
+			Env.add.setAddable(true);
 		});
 		$(".memo-button.important").click(function(e){
+			e.stopPropagation();
 			e.preventDefault();
 			var $this = $(e.currentTarget);
 			if($this.parent().parent().data("readonly") !== "true"){
@@ -47,14 +140,16 @@ $("document").ready(function(){
 					$this.data("checked", "true");				
 				}
 			}
-			
+			Env.focus.removeFocus();
 		});	
 		$(".memo-button.remove").click(function(e){
+			e.stopPropagation();
 			e.preventDefault();
 			var $this = $(e.currentTarget);
 			var $memo = $this.parent().parent();
 			var $span = $memo.find(".memo-content span");
 			if($span.text() === ""){
+				$memo.unbind();
 				$memo.remove();
 			} else {
 				$span.addClass("stroke-out");
@@ -62,19 +157,22 @@ $("document").ready(function(){
 				$memo.data("remove", "true");
 				$memo.data("readonly", "true");
 			}
+			Env.focus.removeFocus();
 		});
 	};
 	
 	var moveTo = function($dom, x, y){
 		$dom.css({marginLeft : calX(x), marginTop : calY(y)});
+		console.log("move To (" + x +", " + y + ")");
 	};
 	
 	var addNewMemo = function(x, y){
-		var innerX = calX(x);
-		var innerY = calY(y);
+// 		var innerX = calX(x);
+// 		var innerY = calY(y);
 		var $cloneMemoSample = $($memoSample.html());
-		$cloneMemoSample.css({marginLeft: innerX, marginTop: innerY});
-		
+		moveTo($cloneMemoSample, x, y);
+// 		$cloneMemoSample.css({marginLeft: innerX, marginTop: innerY});
+		$cloneMemoSample.data("id", "Memo"+Env.id.nextMemoId());
 		var $datepicker = $cloneMemoSample.find(".datepicker");
 		var minDate = new Date();
 	    $datepicker.datepicker({
@@ -94,5 +192,14 @@ $("document").ready(function(){
 	};
 	var calY = function(y){
 		return (y + 156 <= 800 ? (y < 6 ? 6 : y) : y - 156);
-	}
+	};
+	
+	var drawLine = function($from, $to){
+		var fromX = $from.css("margin-left"), fromY = $from.css("margin-top");
+		console.log("from (" + fromX + ", " + fromY + ")");
+		var toX = $to.css("margin-left"), toY = $to.css("margin-top");
+		console.log("to (" + toX + ", " + toY + ")");
+		var $div = $("<div class='line'></div>");
+		
+	};
 })
